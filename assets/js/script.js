703f056b6a6338c6b6aea1442e6b4d55362f7659
nikkikhou <nikkikhou@gmail.com>
2022-10-04 16:12:05 -0400

    inital commit; added start page to start quiz

---
+++ assets/js/script.js
@@ -0,0 +1,95 @@
+var myQuestions = [
+  {
+    question: "What does HTML Stand for?",
+    options: [
+      "Hyper Transfer Machine Language",
+      "Hyper Text Markup Language",
+      "Hidden Text Meta List",
+      "Heavy Transfer Machine List",
+    ],
+    answer: "Hyper Text Markup Language",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store _______.",
+    options: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
+  {
+    question:
+      "String values must be enclosed within ______ when being assigned to variables.",
+    options: ["commas", "curly brackets", "quotes", "Parenthesis"],
+    answer: "curly brackets",
+  },
+  {
+    question: "What does CSS stand for?",
+    options: [
+      "Colorful Style Sheets",
+      "Cascading Sheet Style",
+      "Commanding Sheet Style",
+      "Cascading Style Sheets",
+    ],
+    answer: "Cascading Style Sheets",
+  },
+  {
+    question:
+      "In JavaScript, a ______ is a sequence of one or more characters that may consist of letters, numbers, or symbols?",
+    options: ["color", "array", "string", "boolean"],
+    answer: "string",
+  },
+  {
+    question: "What does DOM stand for?",
+    options: [
+      "Document Object Model",
+      "Dad or Mom",
+      "Document Order Model",
+      "Do Order Model",
+    ],
+    answer: "Document Object Model",
+  },
+  {
+    question: "What does HTML Stand for?",
+    options: [
+      "Hyper Transfer Machine Language",
+      "Hyper Text Markup Language",
+      "Hidden Text Meta List",
+      "Heavy Transfer Machine List",
+    ],
+    answer: "Hyper Text Markup Language",
+  },
+  {
+    question: "What does HTML Stand for?",
+    options: [
+      "Hyper Transfer Machine Language",
+      "Hyper Text Markup Language",
+      "Hidden Text Meta List",
+      "Heavy Transfer Machine List",
+    ],
+    answer: "Hyper Text Markup Language",
+  },
+  {
+    question: "What does HTML Stand for?",
+    options: [
+      "Hyper Transfer Machine Language",
+      "Hyper Text Markup Language",
+      "Hidden Text Meta List",
+      "Heavy Transfer Machine List",
+    ],
+    answer: "Hyper Text Markup Language",
+  },
+  {
+    question:
+      "______ is a form of data with only two possible values, usually true or false.",
+    options: ["Function", "Boolean", "Array", "List"],
+    answer: "Boolean",
+  },
+];
+
+var startQuizButton = document.querySelector("#start-quiz-button")
+startQuizButton.addEventListener("click", function () {
+  alert("Starting quiz now.");
+});
