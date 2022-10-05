bae46ddfeee4b314c9f0524f45f5a7a082201fab
nikkikhou <nikkikhou@gmail.com>
2022-10-04 21:07:10 -0400

    added functions to stop timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var myQuestions = [
   {
-    question: "What does HTML Stand for?",
+    question: "1. What does HTML Stand for?",
     options: [
       "Hyper Transfer Machine Language",
       "Hyper Text Markup Language",
@@ -10,7 +10,7 @@ var myQuestions = [
     answer: "Hyper Text Markup Language",
   },
   {
-    question: "Arrays in JavaScript can be used to store _______.",
+    question: "2. Arrays in JavaScript can be used to store _______.",
     options: [
       "numbers and strings",
       "other arrays",
@@ -21,12 +21,12 @@ var myQuestions = [
   },
   {
     question:
-      "String values must be enclosed within ______ when being assigned to variables.",
+      "3. String values must be enclosed within ______ when being assigned to variables.",
     options: ["commas", "curly brackets", "quotes", "Parenthesis"],
     answer: "curly brackets",
   },
   {
-    question: "What does CSS stand for?",
+    question: "4. What does CSS stand for?",
     options: [
       "Colorful Style Sheets",
       "Cascading Sheet Style",
@@ -37,12 +37,12 @@ var myQuestions = [
   },
   {
     question:
-      "In JavaScript, a ______ is a sequence of one or more characters that may consist of letters, numbers, or symbols?",
+      "5. In JavaScript, a ______ is a sequence of one or more characters that may consist of letters, numbers, or symbols?",
     options: ["color", "array", "string", "boolean"],
     answer: "string",
   },
   {
-    question: "What does DOM stand for?",
+    question: "6. What does DOM stand for?",
     options: [
       "Document Object Model",
       "Dad or Mom",
@@ -52,53 +52,55 @@ var myQuestions = [
     answer: "Document Object Model",
   },
   {
-    question: "What does HTML Stand for?",
-    options: [
-      "Hyper Transfer Machine Language",
-      "Hyper Text Markup Language",
-      "Hidden Text Meta List",
-      "Heavy Transfer Machine List",
-    ],
-    answer: "Hyper Text Markup Language",
-  },
-  {
-    question: "What does HTML Stand for?",
-    options: [
-      "Hyper Transfer Machine Language",
-      "Hyper Text Markup Language",
-      "Hidden Text Meta List",
-      "Heavy Transfer Machine List",
-    ],
-    answer: "Hyper Text Markup Language",
-  },
-  {
-    question: "What does HTML Stand for?",
-    options: [
-      "Hyper Transfer Machine Language",
-      "Hyper Text Markup Language",
-      "Hidden Text Meta List",
-      "Heavy Transfer Machine List",
-    ],
-    answer: "Hyper Text Markup Language",
+    question:
+      "7. The condition in an if/else statement is enclosed with ______.",
+    options: ["quotes", "curly brackets", "paranthesis", "square brackets"],
+    answer: "paranthesis",
   },
   {
     question:
-      "______ is a form of data with only two possible values, usually true or false.",
+      "8. A(n) _______ is used to perform the same action multiple times in a row.",
+    options: ["loop", "array", "functiont", "string"],
+    answer: "loop",
+  },
+  {
+    question:
+      "9. A ________ is a set of features and rules that exist inside a software program (the application) enabling interaction with it through software?",
+    options: ["CSS", "JavaScript", "HTML", "API"],
+    answer: "API",
+  },
+  {
+    question:
+      "10. ______ is a form of data with only two possible values, usually true or false.",
     options: ["Function", "Boolean", "Array", "List"],
     answer: "Boolean",
   },
 ];
 
-var startQuizButton = document.querySelector("#start-quiz-button")
+// quiz button
+var startQuizButton = document.querySelector("#start-quiz-button");
 startQuizButton.addEventListener("click", function () {
-  alert("Starting quiz now.");
+  subtractTime();
 });
 
+// timer
 var timeLeft = 75;
-var quizTimerEl = document.querySelector("#quiz-timer")
-var quizTimerInterval = setInterval(function() {
-   quizTimerEl.innerHTML = `${timeLeft} seconds left`;
-   timeLeft--;
+var quizTimerEl = document.querySelector("#quiz-timer");
+var quizTimerInterval = setInterval(function () {
+  timeLeft--;
+  if (timeLeft === 0) stopTime();
+  quizTimerEl.innerHTML = `${timeLeft} seconds left`;
+}, 1000);
 
-}, 1000)
+// stops timer
+var stopTime = function () {
+  clearInterval(quizTimerInterval);
+};
 
+// subtracting time
+var subtractTime = function () {
+  timeLeft = timeLeft - 10;
+  if (timeLeft < 0) {
+    timeLeft = 0
+  }
+};
