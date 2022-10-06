72484b0f36ef905e05ce2c93d8bf876f37855c02
nikkikhou <nikkikhou@gmail.com>
2022-10-06 15:39:13 -0400

    displayed possible answer for each question

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,27 +2,27 @@
 var startQuizButtonEl = document.querySelector("#start-quiz-button");
 startQuizButtonEl.addEventListener("click", function () {
   startQuiz();
-})
+});
 
 // answer button listener
-var answerButtonEl = document.querySelector("#questions ol button");
+var answerButtonEl = document.querySelector("#answer-buttons button");
 answerButtonEl.addEventListener("click", function () {
   nextQuestion();
-})
+});
 
 // start quiz
-function startQuiz () {
-    var introEl = document.querySelector("#intro");
-    introEl.classList.add("hide")
-    var questionEl = document.querySelector("#questions");
-    questionEl.classList.remove("hide")
-    showQuestion();
+function startQuiz() {
+  var introEl = document.querySelector("#intro");
+  introEl.classList.add("hide");
+  var questionEl = document.querySelector("#questions");
+  questionEl.classList.remove("hide");
+  showQuestion();
 }
 
 // next question
-function nextQuestion () {
-    currentQuestionIndex ++
-    showQuestion()
+function nextQuestion() {
+  currentQuestionIndex++;
+  showQuestion();
 }
 
 // timer
@@ -43,101 +43,104 @@ var stopTime = function () {
 var subtractTime = function () {
   timeLeft = timeLeft - 10;
   if (timeLeft < 0) {
-    timeLeft = 0
+    timeLeft = 0;
   }
 };
 
-// this is displaying current question to the question section. 
+// this is displaying current question to the question section.
 var currentQuestionIndex = 0;
-var showQuestion = function () {   
-    var currentQuestion = myQuestions [currentQuestionIndex] 
-    var questionAskedEl = document.querySelector("#questions h2");
-    questionAskedEl.innerHTML = currentQuestion.question
-}
-var showAnswer = function () {
-    var correctAnswer = answer
-}
+var showQuestion = function () {
+  var currentQuestion = myQuestions[currentQuestionIndex];
+  var questionAskedEl = document.querySelector("#questions h2");
+  questionAskedEl.innerHTML = currentQuestion.question;
 
 
+  // Get the <ol> object from the DOM
+  var answers = document.querySelector("#answer-buttons").children;
 
-
+  // Loop through the children of the <ol>
+  for (i = 0; i < answers.length; i++) {
+    answers[i].innerHTML = currentQuestion.options[i];
+    
+  }
+};
 
 // questions
 var myQuestions = [
-    {
-      question: "1. What does HTML Stand for?",
-      options: [
-        "Hyper Transfer Machine Language",
-        "Hyper Text Markup Language",
-        "Hidden Text Meta List",
-        "Heavy Transfer Machine List",
-      ],
-      answer: "Hyper Text Markup Language",
-    },
-    {
-      question: "2. Arrays in JavaScript can be used to store _______.",
-      options: [
-        "numbers and strings",
-        "other arrays",
-        "booleans",
-        "all of the above",
-      ],
-      answer: "all of the above",
-    },
-    {
-      question:
-        "3. String values must be enclosed within ______ when being assigned to variables.",
-      options: ["commas", "curly brackets", "quotes", "Parenthesis"],
-      answer: "curly brackets",
-    },
-    {
-      question: "4. What does CSS stand for?",
-      options: [
-        "Colorful Style Sheets",
-        "Cascading Sheet Style",
-        "Commanding Sheet Style",
-        "Cascading Style Sheets",
-      ],
-      answer: "Cascading Style Sheets",
-    },
-    {
-      question:
-        "5. In JavaScript, a ______ is a sequence of one or more characters that may consist of letters, numbers, or symbols?",
-      options: ["color", "array", "string", "boolean"],
-      answer: "string",
-    },
-    {
-      question: "6. What does DOM stand for?",
-      options: [
-        "Document Object Model",
-        "Dad or Mom",
-        "Document Order Model",
-        "Do Order Model",
-      ],
-      answer: "Document Object Model",
-    },
-    {
-      question:
-        "7. The condition in an if/else statement is enclosed with ______.",
-      options: ["quotes", "curly brackets", "paranthesis", "square brackets"],
-      answer: "paranthesis",
-    },
-    {
-      question:
-        "8. A(n) _______ is used to perform the same action multiple times in a row.",
-      options: ["loop", "array", "functiont", "string"],
-      answer: "loop",
-    },
-    {
-      question:
-        "9. A ________ is a set of features and rules that exist inside a software program (the application) enabling interaction with it through software?",
-      options: ["CSS", "JavaScript", "HTML", "API"],
-      answer: "API",
-    },
-    {
-      question:
-        "10. ______ is a form of data with only two possible values, usually true or false.",
-      options: ["Function", "Boolean", "Array", "List"],
-      answer: "Boolean",
-    },
-  ];
+  {
+    question: "1. What does HTML Stand for?",
+    options: [
+      "Hyper Transfer Machine Language",
+      "Hyper Text Markup Language",
+      "Hidden Text Meta List",
+      "Heavy Transfer Machine List",
+    ],
+    answer: "Hyper Text Markup Language",
+  },
+  {
+    question: "2. Arrays in JavaScript can be used to store _______.",
+    options: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
+  {
+    question:
+      "3. String values must be enclosed within ______ when being assigned to variables.",
+    options: ["commas", "curly brackets", "quotes", "Parenthesis"],
+    answer: "curly brackets",
+  },
+  {
+    question: "4. What does CSS stand for?",
+    options: [
+      "Colorful Style Sheets",
+      "Cascading Sheet Style",
+      "Commanding Sheet Style",
+      "Cascading Style Sheets",
+    ],
+    answer: "Cascading Style Sheets",
+  },
+  {
+    question:
+      "5. In JavaScript, a ______ is a sequence of one or more characters that may consist of letters, numbers, or symbols?",
+    options: ["color", "array", "string", "boolean"],
+    answer: "string",
+  },
+  {
+    question: "6. What does DOM stand for?",
+    options: [
+      "Document Object Model",
+      "Dad or Mom",
+      "Document Order Model",
+      "Do Order Model",
+    ],
+    answer: "Document Object Model",
+  },
+  {
+    question:
+      "7. The condition in an if/else statement is enclosed with ______.",
+    options: ["quotes", "curly brackets", "paranthesis", "square brackets"],
+    answer: "paranthesis",
+  },
+  {
+    question:
+      "8. A(n) _______ is used to perform the same action multiple times in a row.",
+    options: ["loop", "array", "functiont", "string"],
+    answer: "loop",
+  },
+  {
+    question:
+      "9. A ________ is a set of features and rules that exist inside a software program (the application) enabling interaction with it through software?",
+    options: ["CSS", "JavaScript", "HTML", "API"],
+    answer: "API",
+  },
+  {
+    question:
+      "10. ______ is a form of data with only two possible values, usually true or false.",
+    options: ["Function", "Boolean", "Array", "List"],
+    answer: "Boolean",
+  },
+];
